1cb377dafba3d4ec9f3668ba6dbc581bc2f7d697
津田 翔大 <syota.t@gmail.com>
2021-02-13 05:38:21 +0900

    before object pool

---
+++ Assets/Scripts/ObjectPool/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+  private Rigidbody _Rigidbody;
+
+  private void Start()
+  {
+    _Rigidbody = GetComponent<Rigidbody>();
+
+    // 衝突したら削除
+    this.OnTriggerEnterAsObservable()
+      .Subscribe(_ => Destroy(gameObject))
+    ;
+    // 3秒経過したら削除
+    Destroy(gameObject, 3.0f);
+  }
+
+  public void AddVelocity(Vector3 pVelocity)
+  {
+    Observable.NextFrame(FrameCountType.FixedUpdate)
+      .Subscribe(_ => _Rigidbody.AddForce(pVelocity, ForceMode.VelocityChange))
+      .AddTo(this)
+    ;
+  }
+}
---
+++ Assets/Scripts/ObjectPool/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+
+public class Enemy : MonoBehaviour
+{
+  [SerializeField]
+  private Bullet m_PrefabBullet;
+
+  private void Start()
+  {
+    // 1秒毎に弾を発射
+    Observable
+      .Interval(TimeSpan.FromSeconds(1))
+      .Subscribe(_ => ShootBullets())
+      .AddTo(this)
+    ;
+  }
+
+  private void ShootBullets()
+  {
+    for (int i = -1; i < 2; i++)
+    {
+      Bullet _Bullet = Instantiate<Bullet>(m_PrefabBullet);
+
+      // 3way
+      Vector3 _Direction = Quaternion.AngleAxis(i * 30.0f, transform.up) * transform.forward;
+      _Bullet.transform.position += _Direction * 1.0f;
+      _Bullet.AddVelocity(_Direction * 3.0f);
+    }
+  }
+}

4b67c08ca74c7522164dc3b8fe487e41086a07cf
Syota Tsuda <s-tsuda@magicant.co.jp>
2021-03-25 19:36:16 +0900

    update scene setting for URP

--- Assets/Scripts/ObjectPool/Bullet.cs
+++ Assets/Scripts/ObjectPool/Bullet.cs
@@ -6,53 +6,53 @@ using UniRx.Triggers;
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
-  private Rigidbody m_Rigidbody;
+    private Rigidbody m_Rigidbody;
 
-  private readonly Subject<Unit> m_OnFinishedSubject = new Subject<Unit>();
-  public IObservable<Unit> OnFinishedSubject { get { return m_OnFinishedSubject.AsObservable(); } }
+    private readonly Subject<Unit> m_OnFinishedSubject = new Subject<Unit>();
+    public IObservable<Unit> OnFinishedSubject { get { return m_OnFinishedSubject.AsObservable(); } }
 
-  private void Start()
-  {
-    m_Rigidbody = GetComponent<Rigidbody>();
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
 
-    // 衝突
-    this.OnTriggerEnterAsObservable()
-      .Subscribe(_ => OnHit())
-    ;
-  }
+        // 衝突
+        this.OnTriggerEnterAsObservable()
+          .Subscribe(_ => OnHit())
+        ;
+    }
 
-  public void Initialize(Vector3 pPosition, Vector3 pVelocity)
-  {
-    transform.position = pPosition;
+    public void Initialize(Vector3 pPosition, Vector3 pVelocity)
+    {
+        transform.position = pPosition;
 
-    Observable
-      .NextFrame(FrameCountType.FixedUpdate)
-      .TakeUntilDisable(this)
-      .Subscribe(_ => m_Rigidbody.AddForce(pVelocity, ForceMode.VelocityChange))
-      .AddTo(this)
-    ;
+        Observable
+          .NextFrame(FrameCountType.FixedUpdate)
+          .TakeUntilDisable(this)
+          .Subscribe(_ => m_Rigidbody.AddForce(pVelocity, ForceMode.VelocityChange))
+          .AddTo(this)
+        ;
 
-    Observable
-      .Timer(TimeSpan.FromSeconds(3))
-      .TakeUntilDisable(this)
-      .TakeUntilDestroy(this)
-      .Subscribe(_ => Finish())
-    ;
-  }
+        Observable
+          .Timer(TimeSpan.FromSeconds(3))
+          .TakeUntilDisable(this)
+          .TakeUntilDestroy(this)
+          .Subscribe(_ => Finish())
+        ;
+    }
 
-  private void OnHit()
-  {
-    Finish();
-  }
+    private void OnHit()
+    {
+        Finish();
+    }
 
-  private void Finish()
-  {
-    m_Rigidbody.velocity = Vector3.zero;
-    m_OnFinishedSubject.OnNext(Unit.Default);
-  }
+    private void Finish()
+    {
+        m_Rigidbody.velocity = Vector3.zero;
+        m_OnFinishedSubject.OnNext(Unit.Default);
+    }
 
-  private void OnDestroy()
-  {
-    m_OnFinishedSubject.Dispose();
-  }
+    private void OnDestroy()
+    {
+        m_OnFinishedSubject.Dispose();
+    }
 }
